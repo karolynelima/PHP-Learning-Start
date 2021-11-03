2bc65b8e91acaaf6220b4477e5c8a706881db9ce
karolynelima <k.karolyne.e@hotmail.com>
2021-11-03 15:28:26 -0300

    atualização das pastas

--- functions/functions-parameters-includes.php
+++ functions/functions-parameters-includes.php
@@ -1,14 +1,24 @@
 <?php
+/*
+FUNÇÃO QUE LISTA OS NÚMEROS DE 0 A 9
+*/
 function subsequente(){
     for($q=0;$q<10;$q++){
         echo $q."<br/>";
     }
     echo "<hr/>";
 }
-
 subsequente();
 
-
+/*
+FUNÇÃO DE SOMA
+*/
+function somar($n1, $n2){
+    $total = $n1 + $n2;
+    return $total;
+}
+$soma = somar(10, 5);
+echo "TOTAL :".$soma;
 
 
 

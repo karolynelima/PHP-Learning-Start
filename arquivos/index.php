9a3bc2fa4cc44acbe149f6fc27d04ef8767e9155
karolynelima <k.karolyne.e@hotmail.com>
2021-11-10 11:22:23 -0300

    Renomear, Mover, Copiar

--- arquivos/index.php
+++ arquivos/index.php
@@ -1,15 +1,38 @@
 <?php
-    //LEITURA DE UM ARQUIVO INTERNO;
+    /*
+    FILE_GET_CONTENTS('path to file'): MÉTODO PARA LEITURA DE UM ARQUIVO O TRANSFORMANDO EM STRING;
+    */
     $leitura = file_get_contents('text.txt');
     echo $leitura;
     echo "<hr/ >";
 
-    //ESCREVER EM ARQUIVOS;
+    /*
+    FILE_PUT_CONTENTS('path to file', $string): MÉTODO PARA ESCRITA EM UM ARQUIVO;
+    */
     $escrita = 'KAROLYNE LIMA';
     file_put_contents('nome.txt', $escrita);
     echo 'Arquivo criado com sucesso!';
     echo "<hr/ >";
 
-    //EXCLUIR UM ARQUIVO;
-    unlink('delete.txt');    
+    /*
+    UNLINK('path to file'): MÉTODO PARA EXCLUSÃO DE UM ARQUIVO;
+    unlink('delete.txt');
+    */    
+
+    /*
+    RENAME('path to file','novo nome'): MÉTODO PARA RENOMEAR UM ARQUIVO;
+    rename('rename.txt', 'renomeado.txt');
+    */    
+    
+    /*
+    RENAME('path to file', 'new path'): MÉTODO PARA MOVER UM ARQUIVO PARA OUTRA PASTA; 
+    rename('nome.txt', '../loop/nome.txt');
+    */
+    
+    /*
+    COPY('path to file','new path/name.txt'): MÉTODO PARA COPIAR UM ARQUIVO;
+    copy('text.txt','../teste.txt');
+    */
 ?>
+
+

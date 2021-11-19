6a3d7fa214168a57adcf242ac3a68af520db2719
karolynelima <k.karolyne.e@hotmail.com>
2021-11-19 15:22:34 -0300

    Polimorfismo

--- POO/polimorfismo.php
+++ POO/polimorfismo.php
@@ -1,11 +1,40 @@
 <?php
-class Quadrado{
-
+interface Forma{
+    public function getTipo();    
+    public function getArea();
 }
+
+class Quadrado implements Forma{
+    private $largura;
+    private $altura;
+
+    public function __construct($larg, $altu){
+        $this->largura = $larg;
+        $this->altura  = $altu;
+    }    
+    
+    public function getTipo(){
+        return 'quadrado';
+    }
+    public function getArea(){
+        return ($this->largura * $this->altura);
+    }
+}
+
 class Circulo{
+    private $raio;
+
+    public function __construct($rai){
+        $this->raio = $rai;
+    }
+    public function getTipo(){
+        return 'circulo';
+    }
+    public function getArea(){
+        return pi() * ($this->raio * $this->raio);
+    }
 
 }
-
 
 $quadrado = new Quadrado(5, 5);
 $circulo = new Circulo(7);
@@ -14,12 +43,13 @@ $formas = [
     $quadrado,
     $circulo
 ];
-
+/*
+POLIMORFISMO: VÁRIAS CLASSES DIFERENTES POSSUEM O MESMO METODO;
+              DIZ RESPEITO À IMPLEMENTAÇÃO;  
+*/
 foreach($formas as $formas){
-    $tipo = $objeto->getTipo();
-    $area = $objeto->getArea();
+    $tipo = $formas->getTipo();
+    $area = $formas->getArea();
     echo "AREA: ".$tipo." : ".$area."<br/>";
 }
-
-
 ?>
